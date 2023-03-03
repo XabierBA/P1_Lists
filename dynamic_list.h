130355a0eddc7d36fb223fbf7d844def73ac2ce8
XabierBA <xabitic1@gmail.com>
2023-03-03 13:13:20 +0100

    Skeleton v2

--- dynamic_list.h
+++ dynamic_list.h
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Xabier Barreiro Alberte LOGIN 1: xabier.barreiroa@udc.com
+ * AUTHOR 2: Mateo Pérez Rodríguez   LOGIN 2: mateo.perez@udc.es
+ * GROUP: 4.2
+ * DATE: 03 / 03 / 2023
  */
 
 #ifndef DYNAMIC_LIST_H
