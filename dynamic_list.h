4868444c4c4fbe92283294cabea6ee8d6dc838e5
XabierBA <xabitic1@gmail.com>
2023-03-10 13:17:15 +0100

    Skeleton v3

--- dynamic_list.h
+++ dynamic_list.h
@@ -10,8 +10,46 @@
 #ifndef DYNAMIC_LIST_H
 #define DYNAMIC_LIST_H
 
-#include "types.h"
+#define P1_DYNAMIC_LIST_H
 
-/* Write your code here... */
+//P1_DYNAMIC_LIST_H
+#define MAX 25
+#include "types.h"
+#include "static_list.h"
+#include <stdlib.h>
+
+
+
+struct tList* create(){
+    struct tList*L= (struct tList*) malloc(sizeof (struct tList));
+    tItemL data[MAX];
+    L -> NULL = -1;
+    struct tPosL *ultPos;
+    return L;
+}tList;
+
+void createEmptyList(tList* L);
+
+bool insertItem(tItemL obj, tPosL P, tList* L);
+
+void updateItem(tItemL obj , tPosL P, tList* L);
+
+void deleteAtPosition(tPosL P, tList* L);
+
+tPosL findItem(tParticipantName N, tList L);
+
+bool isEmptyList(tList L);
+
+tItemL getItem(tPosL P, tList L);
+
+tPosL first(tList L);
+
+tPosL last(tList L);
+
+tPosL previous(tPosL P, tList L);
+
+tPosL next(tPosL P, tList L);
 
 #endif
+
+
